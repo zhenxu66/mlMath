15916f8d072d6d15f7efbf43ad2280021e5ffd68
zhenxu <zhenxu66@gmail.com>
2019-07-23 10:33:00 -0700

    Initial commit

---
+++ LA/LA_basic.py
@@ -0,0 +1,12 @@
+from __future__ import division, print_function, unicode_literals
+
+# NumPy's ndarray provides a lot of convenient and optimized implementations of
+# essential mathematical operations on vectors
+
+# 1.Vectors
+import numpy as np
+array1 = np.array([1, 2, 3, 4])
+print(array1.size)
+print(array1.shape)
+print(array1[0])
+
